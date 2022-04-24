726838249dcc786269615e19c7615330212e649b
Potrijo <backinmarc@gmail.com>
2022-04-24 22:11:55 +0200

    Funciona JPA 100%

--- Jpa1/src/main/java/com/marc/jpa1/Jpa1.java
+++ Jpa1/src/main/java/com/marc/jpa1/Jpa1.java
@@ -65,14 +65,41 @@ public class Jpa1 {
 		 //Commit transaction
 		 manager.getTransaction().commit();
 		 }
+	 
+	 public static void verTodos() {
+		 System.out.println("Mostrando todos los datos:");
+		 Query consulta = manager.createQuery("Select l "
+		 + "from Libros l");
+		 List resultados = consulta.getResultList();
+		 for (Object resultado : resultados) {
+			 Libros libro = (Libros) resultado;
+			 System.out.println(libro.getId() + ": "
+			 + libro.getTitulo() + ", de "
+			 + libro.getAutor());
+		 }
+	 }
+	 
+	 public static void verUnTitulo() {
+		 System.out.println("Mostrando todos los datos:");
+		 Libros libro = manager
+		 .createNamedQuery( "Libros.findByTitulo", Libros.class )
+		 .setParameter( "titulo", "Macbeth" )
+		 .getSingleResult();
+		 System.out.println(libro.getId() + ": "
+		 + libro.getTitulo() + ", de "
+		 + libro.getAutor());
+	 }
+
 
 	
 	public static void main(String[] args) {
 		tearUp();
 		//persist(); // crea los campos
 		//merge(); // actualiza los campos
-		remove();
-		find();
+		//remove(); // borra una fila
+		//verTodos(); // muestra todas los registros de la tabla
+		//find(); // busca un registro específico (por ID)
+		verUnTitulo(); // busca un registro filtrando por titulo
 		tearDown();
 	}
 }
